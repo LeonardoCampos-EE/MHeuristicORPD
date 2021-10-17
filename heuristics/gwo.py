45fb75905844bb833d3ecfe6b0c179d09389a74e
Leonardo <leonardopacampos@gmail.com>
2021-10-17 15:40:15 -0300

    Created update_agents function on GWO

--- heuristics/gwo.py
+++ heuristics/gwo.py
@@ -1,6 +1,10 @@
-from heuristics.optimizer import *
 import time
 from tqdm import tqdm
+import pdb
+
+import numpy as np
+
+from heuristics.optimizer import Optimizer
 
 
 class GWO(Optimizer):
@@ -34,9 +38,6 @@ class GWO(Optimizer):
         self.best_constraint = []
         self.best_fitness = []
 
-        # Initialize the best solutions, i. e., the alpha, beta and delta wolves
-        self.best_agents = np.zeros(shape=(self.dim, 3), dtype=np.float64)
-
         # Main loop
         start_time = time.time()
         with tqdm(
@@ -47,21 +48,21 @@ class GWO(Optimizer):
             for t in tqdm(range(iterations)):
 
                 # a(t) -> goes from 2 to 0 over the iterations
-                a = 2 - (t * (2.0 / iterations))
+                self.a = 2 - (t * (2.0 / iterations))
 
                 # r1 and r2 -> random numbers between 0 and 1
-                r1 = self.random_gen.random(
+                self.r1 = self.random_gen.random(
                     size=(self.dim, self.population_size), dtype=np.float64
                 )
-                r2 = self.random_gen.random(
+                self.r2 = self.random_gen.random(
                     size=(self.dim, self.population_size), dtype=np.float64
                 )
 
                 # A(t) -> controls the step size of each wolf in the search space
-                A_t = 2 * a * r1 - a
+                self.A_t = 2 * self.a * self.r1 - self.a
 
                 # C(t) -> controls the movement of each wolf towards the best solutions
-                C_t = 2 * r2
+                self.C_t = 2 * self.r2
 
                 # Calculate the objective function
                 self.objective_array = self.objective_function(self.pop_array)
@@ -81,73 +82,41 @@ class GWO(Optimizer):
                 # Get the indexes that would sort the fitness_array to get the best solutions of this iteration
                 sort_indexes = np.argsort(self.fitness_array)
 
-                # Sort each array with respect to sort_indexes to match the alfa, beta, delta, omega wolves format
-                self.objective_array = np.take_along_axis(
-                    self.objective_array, sort_indexes, axis=0
-                )
-                self.constraint_array = np.take_along_axis(
-                    self.constraint_array, sort_indexes, axis=0
-                )
-                self.fitness_array = np.take_along_axis(
-                    self.fitness_array, sort_indexes, axis=0
-                )
-
-                # sort_indexes = np.expand_dims(sort_indexes, axis=0)
-                sort_indexes = np.reshape(sort_indexes, (1, -1))
-                self.pop_array = np.take_along_axis(
-                    self.pop_array, sort_indexes, axis=1
-                )
+                self.alpha_index = sort_indexes[0]
+                self.beta_index = sort_indexes[1]
+                self.delta_index = sort_indexes[2]
 
                 # Keep track of alpha's objective, penalty and fitness functions
-                self.best_objective.append(self.objective_array[0])
+                self.best_objective.append(self.objective_array[self.alpha_index])
                 if self.constraints:
-                    self.best_constraint.append(self.constraint_array[0])
+                    self.best_constraint.append(self.constraint_array[self.alpha_index])
                 else:
                     self.best_constraint.append([0])
-                self.best_fitness.append(self.fitness_array[0])
+                self.best_fitness.append(self.fitness_array[self.alpha_index])
 
-                tq.set_description(f"Best fitness at t = {t}: {self.fitness_array[0]}")
+                tq.set_description(
+                    f"Best fitness at t = {t}: {self.fitness_array[self.alpha_index]}"
+                )
 
-                # Save the best agents for the current iteration
-                self.best_agents = self.pop_array[:, :3]
+                # Update the agents positions
+                self.update_agents()
 
                 # Save the population and the best agents for demonstrantion
                 self.pop_list.append(
                     np.vstack([self.pop_array.copy(), self.fitness_array])
                 )
                 self.best_agents_list.append(
-                    np.vstack([self.best_agents.copy(), self.fitness_array[:3]])
+                    np.vstack(
+                        [
+                            self.pop_array[
+                                :, [self.alpha_index, self.beta_index, self.delta_index]
+                            ],
+                            self.fitness_array[
+                                [self.alpha_index, self.beta_index, self.delta_index]
+                            ],
+                        ]
+                    )
                 )
-
-                # Calculate D_alpha, D_beta, D_delta
-                D_alpha = np.abs(
-                    C_t * np.reshape(self.best_agents[:, 0], (-1, 1)) - self.pop_array
-                )
-                D_beta = np.abs(
-                    C_t * np.reshape(self.best_agents[:, 1], (-1, 1)) - self.pop_array
-                )
-                D_delta = np.abs(
-                    C_t * np.reshape(self.best_agents[:, 2], (-1, 1)) - self.pop_array
-                )
-
-                # Calculate X_alpha, X_beta, X_delta
-                X_alpha = np.reshape(self.best_agents[:, 0], (-1, 1)) - A_t * D_alpha
-                X_beta = np.reshape(self.best_agents[:, 1], (-1, 1)) - A_t * D_beta
-                X_delta = np.reshape(self.best_agents[:, 2], (-1, 1)) - A_t * D_delta
-
-                # Update population
-                self.pop_array = (X_alpha + X_beta + X_delta) / 3.0
-
-                # Clip the variables to the bounds
-                np.clip(
-                    a=self.pop_array,
-                    a_min=self.lower_bounds,
-                    a_max=self.upper_bounds,
-                    out=self.pop_array,
-                )
-
-                # time.sleep(0.5)
-            # End main loop
 
         # Execution time
         end_time = time.time()
@@ -155,10 +124,45 @@ class GWO(Optimizer):
 
         print("---------------- Solution ----------------")
         print(
-            f"Fitness = {self.fitness_array[0]}; Objective = {self.objective_array[0]}; Constraint = {self.constraint_array[0]}"
+            f"Fitness = {self.fitness_array[self.alpha_index]}; Objective = {self.objective_array[self.alpha_index]}; Constraint = {self.constraint_array[self.alpha_index]}"
         )
-        print(f"Best agent = {self.best_agents[:, 0]}")
+        print(f"Best agent = {self.pop_array[:, self.alpha_index]}")
         print(f"Execution time = {execution_time}")
+
+        return
+
+    def update_agents(self):
+
+        """
+        Update the search agents positions
+
+        """
+        # self.best_agents = self.pop_array[:, :3]
+
+        alpha_wolf = np.expand_dims(self.pop_array[:, self.alpha_index], axis=-1)
+        beta_wolf = np.expand_dims(self.pop_array[:, self.beta_index], axis=-1)
+        delta_wolf = np.expand_dims(self.pop_array[:, self.delta_index], axis=-1)
+
+        # Calculate D_alpha, D_beta, D_delta
+        D_alpha = np.abs(self.C_t * alpha_wolf - self.pop_array)
+        D_beta = np.abs(self.C_t * beta_wolf - self.pop_array)
+        D_delta = np.abs(self.C_t * delta_wolf - self.pop_array)
+
+        # Calculate X_alpha, X_beta, X_delta
+        X_alpha = alpha_wolf - self.A_t * D_alpha
+        X_beta = beta_wolf - self.A_t * D_beta
+        X_delta = delta_wolf - self.A_t * D_delta
+
+        # Update population
+        self.pop_array = (X_alpha + X_beta + X_delta) / 3.0
+
+        # Clip the variables to the bounds
+        np.clip(
+            a=self.pop_array,
+            a_min=self.lower_bounds,
+            a_max=self.upper_bounds,
+            out=self.pop_array,
+        )
 
         return
 
